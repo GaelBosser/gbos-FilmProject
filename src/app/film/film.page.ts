19db6d22c83e6e22dcc21420295667ec05ae33d9
GaelBosser <gael.bosser@ynov.com>
2019-01-11 17:52:36 +0100

    Liste films debut

--- src/app/film/film.page.ts
+++ src/app/film/film.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OmdbServiceService } from '../omdb-service.service';
+
 @Component({
   selector: 'app-film',
   templateUrl: './film.page.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmPage implements OnInit {
 
-  constructor() { }
+  film: any = {};
 
-  ngOnInit() {
+  async getFilm() {
+    await this.api.getFilmByTitle("Harry Potter and the Deathly Hallows: Part 2")
+      .subscribe(res => {
+        console.log(res);
+        this.film = res;
+      }, err => {
+        console.log(err);
+      });
   }
 
+  constructor(public api: OmdbServiceService) { }
+
+  ngOnInit() {
+    this.getFilm();
+  }
 }
