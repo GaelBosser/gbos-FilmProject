e76aca7e83ea006ce8b2437557a66a711891ab5c
GaelBosser <gael.bosser@ynov.com>
2019-03-23 17:43:41 +0100

    Fix monter version Ionic 4.12 + monter de versions des plugins angular et typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { FileChooser } from '@ionic-native/file-chooser/ngx';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicStorageModule } from '@ionic/storage'
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HttpClientModule } from '@angular/common/http';
 import { File } from '@ionic-native/file/ngx';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
